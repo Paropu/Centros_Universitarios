afcc79901c4deaa851d561e03f663347b9c124aa
mpugaeg <mpugaeg@gmail.com>
2015-11-24 23:06:09 +0100

    v0.0.1

--- centros_universitarios/src/centros_universitarios/Gestion.java
+++ centros_universitarios/src/centros_universitarios/Gestion.java
@@ -2,6 +2,7 @@ package centros_universitarios;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class Gestion {
@@ -20,13 +21,13 @@ public class Gestion {
 	public static void cargarFicheroPersonas () {
 		FileInputStream flujo_entrada = null;
 		try {
-			flujo_entrada = new FileInputStream("personas.txt");// Se crea un flujo de datos al fichero.
+			flujo_entrada = new FileInputStream("personas.txt"); // Se crea un flujo de datos al fichero.
 		} 
 		catch (FileNotFoundException NoExisteFichero) { // Si el fichero no existe, salta excepcion y se muestra mensaje por pantalla.
 			System.out.println("Fichero \"personas.txt\" inexistente");
 			System.exit(-1); // Mostrar error en el fichero Avisos.txt ----- ???
 		}
-		Scanner entrada = new Scanner(flujo_entrada);// Se crea un objeto para escanear la linea del fichero
+		Scanner entrada = new Scanner(flujo_entrada); // Se crea un objeto para escanear la linea del fichero
 		String linea = null; // Variable que contendra la informacion escaneada del fichero
 		while (entrada.hasNextLine()) {
 			linea = entrada.nextLine(); // Se escanea la linea.
@@ -34,11 +35,14 @@ public class Gestion {
 			else {} //Se recogen los datos de cada persona.
 		}
 		entrada.close();
+	
+		Alumno alumno0 = new Alumno("44497152H", "Martin", "Puga Egea", new GregorianCalendar(1995, 01, 24), new GregorianCalendar(2000, 11, 20)); //Herramienta.
+	System.out.println(alumno0.toString()); //Herramienta.
 	}	
 }
+
 /* -----NOTAS-----
-
 1.- alumno.txt:
-	1- Añadir "asignaturas superadas" y "docencia recibida".
-
+	1- Añadir a alumno: "asignaturas superadas" y "docencia recibida".
+	2-Terminar de modelar las
  */
--- src/Perro.java
+++
@@ -1,6 +0,0 @@
-
-public class Perro {
-	public static void main(String[] args) {
-
-	}
-}
